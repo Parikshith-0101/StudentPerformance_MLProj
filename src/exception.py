d93474d0dc584b04808479eeebb74cd853921d94
Parikshith-0101 <parikshithm2005@gmail.com>
2025-09-21 13:18:06 +0530

    rectified fcode error

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,7 @@ import sys
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.fcode.co_filename
+    file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in script name:[{1}] line no:[{2}] error message:[{3}]".format(
         file_name,exc_tb.tb_lineno,str(error)
     )
